f99785435e112b3177d6dacb20a896720dfb1bb4
Wojciech Piaskowski <wojciech.m.piaskowski@gmail.com>
2025-02-16 16:36:41 +0100

    Poprawa bezpieczeństwa

--- AdBanner.tsx
+++ AdBanner.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import { Platform } from 'react-native';
 import { View, StyleSheet } from 'react-native';
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
+import Config from 'react-native-config';
+
+const IOS_ADUNITID = Config.IOS_ADUNITID as string;
+const ANDROID_ADUNITID = Config.ANDROID_ADUNITID as string;
 
 const adUnitId = Platform.select({
-    ios: 'ca-app-pub-3054958321666076/5075788191', // Wstaw ID reklamy dla iOS
-    android: 'ca-app-pub-3054958321666076/6616100577', // Wstaw ID reklamy dla Androida
+    ios: IOS_ADUNITID,
+    android: ANDROID_ADUNITID,
   }) || TestIds.BANNER;
 
 const AdBanner: React.FC = () => {
   return (
     <View style={styles.container}>
       <BannerAd 
-        unitId={TestIds.BANNER}
+        unitId={adUnitId}
         size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
         requestOptions={{ requestNonPersonalizedAdsOnly: true }}
       />
