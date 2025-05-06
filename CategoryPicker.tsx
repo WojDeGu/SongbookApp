4c6c926539d5189c01090e3dcb605af9e5f2fb4a
Wojciech Piaskowski <wojciech.m.piaskowski@gmail.com>
2025-05-06 10:11:03 +0200

    Kategorie są bardziej personalizowane

--- CategoryPicker.tsx
+++ CategoryPicker.tsx
@@ -13,7 +13,7 @@ const CategoryPicker: React.FC<CategoryPickerProps> = ({ selectedCategory, onSel
   const { theme } = useTheme();
   const styles = theme === 'light' ? lightStyles(isTablet) : darkStyles(isTablet);
 
-  const categories = ['Inne', 'Msza', 'Uwielbienie', 'Maryjne', 'Do Ducha Świętego', 'Wielkanoc', 'Wielki Post', 'Adwent', 'Salezjanie'];
+  const categories = ['Inne', 'Msza', 'Uwielbienie', 'Maryjne', 'Do Ducha Świętego', 'Wielkanoc', 'Wielki Post', 'Adwent',];
 
   const categoryStyles: Record<
     string,
